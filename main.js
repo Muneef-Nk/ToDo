eeb4b507cd0a7e04103059c669d484b3a173c593
MUHAMMED MUNEEF M <md.muneefnk@gmail.com>
2022-05-24 23:32:27 +0530

    add  Function

--- main.js
+++ main.js
@@ -2,3 +2,20 @@
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('todoContainer');
 let inputField = document.getElementById('inputField');
+
+
+addToDoButton.addEventListener('click', function(){
+    
+    let paragraph = document.createElement('h2');
+     paragraph.classList.add('paragraph-styling');
+     paragraph.innerText = inputField.value;
+     toDoContainer.appendChild(paragraph);
+     inputField.value = "";
+     paragraph.addEventListener('click', function(){
+          paragraph.style.textDecoration = "line-through";
+     })
+     paragraph.addEventListener('dblclick', function(){
+          toDoContainer.removeChild(paragraph);
+     })
+     
+} )
